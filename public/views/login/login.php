9ba6cdbddf584adce6be2522a86404229a4f67f2
Dev_Pebmed <dev_pebmed@MacBook-Pro-de-Alexandre.local>
2020-03-24 09:50:23 -0300

    Creating arquive of login and the form

---
+++ public/views/login/login.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Framework</title>
+</head>
+<body>
+    <form action="#">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" />
+        <label for="senha">Senha:</label>
+        <input type="text" id="senha" name="senha" />
+        <input type="submit" />
+    </form>
+</body>
